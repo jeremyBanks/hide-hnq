50cf297de4f22a58d345aee642327ffd0b9726ef
Jeremy Banks <j@jeremybanks.ca>
2015-12-29 21:18:30 -0600

    Various development, including a link to options below the questions.

---
+++ background-nonpersistent.js
@@ -0,0 +1,6 @@
+chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
+  if (request === 'openOptionsPage') {
+    chrome.runtime.openOptionsPage();
+    sendResponse(null);
+  }
+});
--- content.js
+++ content.js
@@ -12,25 +12,21 @@ const options = {
   
   hideStrings: [
     'Cutting bevel in mesh surface',
-  ]
+  ],
+
+  hideIfAllhidden: true,
+
+  showOptionsLink: true
 }
 
 const hnq = document.querySelector('#hot-network-questions');
+if (!hnq) return;
+
+const items = new Set(Array.from(document.querySelectorAll('li')));
+const visibleItems = options.hideAll ? new Set() : new Set(items);
+const hiddenItems = options.hideAll ? new Set(items) : new Set();
 
 if (!options.hideAll) {
-  const items = new Set(Array.from(document.querySelectorAll('li')));
-  const visibleItems = new Set(items);
-  const hiddenItems = new Set();
-
-  if (!('update_url' in chrome.runtime.getManifest())) {
-    console.log("Debug mode enabled for Hide Hot Network Question on Stack Exchange.");
-  } else {
-    console.debug = function() {};
-    console.log = function() {};
-    console.warning = function() {};
-    console.error = function() {};
-  }
-
   // A model of efficiency.
   for (const item of items) {
     const icon = item.querySelector('.favicon');
@@ -41,36 +37,46 @@ if (!options.hideAll) {
     if (!hide) for (const site of options.hideSites) {
       if (icon && icon.title === site) {
         hide = true;
-        console.debug("Hiding", item, "because icon.title is", site);
       } else if (
         icon && !/ /.test(icon) &&
         icon.classList.contains(`favicon-${site}`)
       ) {
         hide = true;
-        console.debug("Hiding", item, "because icon.classList contains", `favicon-${site}`);
-      } else if (link.hostname === site) {
+      } else if (link && link.hostname === site) {
         hide = true;
-        console.debug("Hiding", item, "because link.hostname is", site);
       }
     }
 
-    if (!hide) for (const keyword of options.hideStrings) {
+    if (!hide && link) for (const keyword of options.hideStrings) {
       // Clbuttic.
       if (link.textContent.indexOf(keyword) > -1) {
-        console.debug("Hiding", item, "because link.textContent contains", keyword);
         hide = true;
       }
     }
-  
+
     if (hide) {
       visibleItems.delete(item);
       hiddenItems.add(item);
       item.dataset.jbsehnqHidden = 'true';
     }
   }
+}
+ 
+if (options.hideIfAllhidden && visibleItems.size > 0) {
+  hnq.dataset.jbsehnqHidden = 'false';
   
-  if (visibleItems.size > 0) {
-    hnq.dataset.jbsehnqHidden = 'false';
+  if (options.showOptionsLink) {
+    const optionsLink = document.createElement('a');
+    optionsLink.href = 'chrome://extensions/?options=jommfgnflipjalbpbgcfghdpoeijpoab';
+    optionsLink.textContent = "question hiding options";
+    optionsLink.title = `${hiddenItems.size} questions were hidden based on your current options for the Hide Hot Network Question on Stack Exchange extension.`;
+    optionsLink.classList.add('jbsehnq-options');
+    optionsLink.addEventListener('click', function(event) {
+      chrome.runtime.sendMessage('openOptionsPage');
+      chrome.runtime.openOptionsPage();
+      event.preventDefault();
+    })
+    hnq.insertBefore(optionsLink, null);
   }
 }
 
